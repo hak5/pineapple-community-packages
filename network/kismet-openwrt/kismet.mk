b978a16984420c5a1b8cb2070f97fdbcb8fa673a
Marc Egerton <foxtrot@realloc.me>
2020-12-08 15:25:17 +0000

    Kismet: Mirror Kismet R3

--- network/kismet-openwrt/kismet.mk
+++ network/kismet-openwrt/kismet.mk
@@ -1,14 +1,24 @@
-PKG_VERSION:=2020-09-R1
+PKG_VERSION:=2020-12-R3
 PKG_RELEASE:=1
 
 PKG_BUILD_DIR:=$(BUILD_DIR)/$(PKG_NAME)-$(PKG_VERSION)
 PKG_SOURCE_PROTO:=git
 PKG_SOURCE_URL:=https://github.com/kismetwireless/kismet.git
-PKG_SOURCE_VERSION:=f2b914ee3089ba5b4d18408cee1a73fe0072e78d
+PKG_SOURCE_VERSION:=kismet-2020-12-R3
 PKG_SOURCE:=$(PKG_NAME)-$(PKG_VERSION)-$(PKG_SOURCE_VERSION).tar.gz
 PKG_SOURCE_SUBDIR:=$(PKG_NAME)-$(PKG_VERSION)
 
 PKG_USE_MIPS16:=0
 
-HOST_BUILD_DEPENDS:=protobuf/host 
+PKG_BUILD_DEPENDS:=protobuf-c/host
 
+CONFIGURE_OPTS := \
+	--sysconfdir=/etc/kismet \
+	--bindir=/usr/bin \
+	--disable-python-tools \
+	--with-protoc=$(STAGING_DIR_HOSTPKG)/bin/protoc \
+	--enable-protobuflite \
+	--disable-element-typesafety \
+	--disable-debuglibs \
+	--disable-libcap \
+	--disable-libwebsockets 
